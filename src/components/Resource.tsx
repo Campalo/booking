e904c069c24eaff7fd1e62348e15d4fe2900a60e
Campalo <25295489+Campalo@users.noreply.github.com>
2023-01-26 17:53:48 +0100

    Check if resource is booked

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -1,21 +1,44 @@
-import { ResourceType } from '../utils/useResource';
+import { format, isWithinInterval } from "date-fns";
+import { useEffect, useState } from "react";
+import { ResourceType } from "../utils/useResource";
 import "./Resource.scss";
 
 interface Props {
   resource: ResourceType | undefined;
 }
 
-const Resource = ({resource}:Props) => {
+const Resource = ({ resource }: Props) => {
+  const [isBookedNow, setIsBookedNow] = useState(false);
+
+  useEffect(() => {
+    if (!!resource?.bookingInfo) {
+      const interval = {start: new Date(resource?.bookingInfo?.start), end: new Date(resource?.bookingInfo?.end)}
+      setIsBookedNow(!!interval && isWithinInterval(new Date(resource?.bookingInfo?.start), interval));
+    }
+  }, [resource?.bookingInfo])
+
   return (
     <header>
       <h1>{resource?.name}</h1>
       <div>
-         <h2>Free</h2>
-        <button onClick={() => console.log("TODO: Book")}>Book now</button>
+        {isBookedNow && resource?.bookingInfo? (
+          <>
+            <h2>Booked</h2>
+            <p>By {resource?.bookingInfo?.userId}</p>
+            <p>From {format(new Date(resource?.bookingInfo?.start), "p")} to {format(new Date(resource?.bookingInfo?.end), "p")}</p>
+          </>
+        ) : (
+          <>
+            <h2>Free</h2>
+            <button onClick={() => console.log("TODO: Book")}>Book now</button>
+            <p>
+              Booking range: from {resource?.minimumBookingDuration} min to {resource?.maximumBookingDuration} min.
+            </p>
+          </>
+        )}
       </div>
-        <p>Book for at least {resource?.minimumBookingDuration} min and for {resource?.maximumBookingDuration} min maximum.</p>
     </header>
-  )
-}
+  );
+};
 
 export default Resource;
--- src/utils/useResource.tsx
+++ src/utils/useResource.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import * as api from "../api/api";
 import { useAuth } from "./useAuth";
+import { BookingType, useBookings } from "./useBookings";
 
 export interface ResourceType {
   id: string;
@@ -8,12 +9,14 @@ export interface ResourceType {
   bookingDurationStep: number;
   maximumBookingDuration: number;
   minimumBookingDuration: number;
+  bookingInfo?: BookingType;
 }
 
 export const useResource = () => {
   const [resource, setResource] = useState<ResourceType>();
   const [error, setError] = useState("");
   const { auth, isLoggedIn} = useAuth();
+  const { bookings } = useBookings();
 
   useEffect(() => {
     if(isLoggedIn) {
@@ -24,6 +27,14 @@ export const useResource = () => {
       .catch(error => setError(error.toString()))
     }
   }, [isLoggedIn, auth]);
+
+  useEffect(() => {
+    if (!!resource) {
+      const bookingInfo =  bookings.find((booking) => booking.id === resource.id);
+      setResource({...resource, bookingInfo});
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [bookings]);
 
   return {resource, resourceError: error};
 }
