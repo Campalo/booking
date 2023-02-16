1989d47d293b98ce65aed154a7e025d584483381
Campalo <25295489+Campalo@users.noreply.github.com>
2023-02-16 16:50:47 +0100

    Display styled booking list

--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -1,3 +1,6 @@
+import { endOfDay, format, startOfDay } from "date-fns";
+import "./BookingList.scss";
+
 export interface BookingType {
   id: string;
   start: Date;
@@ -15,20 +18,50 @@ const BookingList = ({bookings}: Props) => {
     return (<p>No bookings found</p>)
   }
 
-  const list = bookings.map((booking) => {
-    return (
-      <li key={booking.id}>
-        <span>{booking.name}</span>
-      </li>
-    )
-  })
+  let rows = [];
+  let previousEnd = startOfDay(new Date());
+
+  for (const booking of bookings) {
+    // Rows between bookings
+    if (booking.start > previousEnd) {
+      rows.push((
+        <tr key={booking.start.getTime()} className="available">
+          <td><time>{format(previousEnd, "p")}</time> - <time>{format(booking.start, "p")}</time></td>
+          <td>Available</td>
+        </tr>
+      ))
+    }
+
+    // Rows for bookings
+    rows.push((
+      <tr key={booking.end.getTime()} className="booked">
+        <td><time>{format(booking.start, "p")}</time> - <time>{format(booking.end, "p")}</time></td>
+        <td>{booking.name}</td>
+      </tr>
+    ))
+
+    previousEnd = booking.end;
+  }
+
+  // Rows after last booking until end of day
+  const endOfTheDay = endOfDay(new Date());
+  if (endOfTheDay > previousEnd) {
+    rows.push((
+      <tr key={endOfTheDay.getTime()} className="available">
+        <td><time>{format(previousEnd, "p")}</time> - <time>{format(endOfTheDay, "p")}</time></td>
+        <td>Available</td>
+      </tr>
+    ))
+  }
 
   return (
-    <ol>
-      {list}
-    </ol>
+    <table>
+      <caption>Today's bookings</caption>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
   )
-
 };
 
 export default BookingList;
